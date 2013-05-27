150578cedd0f3596210588ab55b963789ad6ee00
Max Jacobson <maxwell.jacobson@gmail.com>
2013-05-27 19:42:41 -0400

    404 js doesnt need to worry about jquery

--- _coffee/404.coffee
+++ _coffee/404.coffee
@@ -1,6 +1,5 @@
-$ ->
-  current_url = document.URL
-  pattern = /(\d{4})\-\d{2}\-\d{2}\-([a-z\-]+)$/i
-  if current_url.match pattern
-    match = current_url.match pattern
-    window.location.replace "http://maxjacobson.github.io/#{match[1]}/#{match[2]}/"
+current_url = document.URL
+pattern = /(\d{4})\-\d{2}\-\d{2}\-([a-z\-]+)$/i
+if current_url.match pattern
+  match = current_url.match pattern
+  window.location.replace "http://maxjacobson.github.io/#{match[1]}/#{match[2]}/"
--- js/404.js
+++ js/404.js
@@ -1,13 +1,13 @@
 (function() {
-  $(function() {
-    var current_url, match, pattern;
+  var current_url, match, pattern;
 
-    current_url = document.URL;
-    pattern = /(\d{4})\-\d{2}\-\d{2}\-([a-z\-]+)$/i;
-    if (current_url.match(pattern)) {
-      match = current_url.match(pattern);
-      return window.location.replace("http://maxjacobson.github.io/" + match[1] + "/" + match[2] + "/");
-    }
-  });
+  current_url = document.URL;
+
+  pattern = /(\d{4})\-\d{2}\-\d{2}\-([a-z\-]+)$/i;
+
+  if (current_url.match(pattern)) {
+    match = current_url.match(pattern);
+    window.location.replace("http://maxjacobson.github.io/" + match[1] + "/" + match[2] + "/");
+  }
 
 }).call(this);
