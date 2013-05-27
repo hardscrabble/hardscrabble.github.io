159c8856b5fdc2476a1313f73c7278c81393a96c
Max Jacobson <maxwell.jacobson@gmail.com>
2013-05-27 19:40:57 -0400

    redirect should work

--- _coffee/404.coffee
+++ _coffee/404.coffee
@@ -1,4 +1,6 @@
 $ ->
-  pattern = /\d{4}\-\d{2}\-\d{2}\-([a-z\-])$/i
   current_url = document.URL
-  console.log current_url.match(pattern)
+  pattern = /(\d{4})\-\d{2}\-\d{2}\-([a-z\-]+)$/i
+  if current_url.match pattern
+    match = current_url.match pattern
+    window.location.replace "http://maxjacobson.github.io/#{match[1]}/#{match[2]}/"
--- js/404.js
+++ js/404.js
@@ -1,10 +1,13 @@
 (function() {
   $(function() {
-    var current_url, pattern;
+    var current_url, match, pattern;
 
-    pattern = /\d{4}\-\d{2}\-\d{2}\-([a-z\-])$/i;
     current_url = document.URL;
-    return console.log(current_url.match(pattern));
+    pattern = /(\d{4})\-\d{2}\-\d{2}\-([a-z\-]+)$/i;
+    if (current_url.match(pattern)) {
+      match = current_url.match(pattern);
+      return window.location.replace("http://maxjacobson.github.io/" + match[1] + "/" + match[2] + "/");
+    }
   });
 
 }).call(this);
