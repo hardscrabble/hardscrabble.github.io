b3158c04fce19e24381242d8c22af5d7ca1082cc
Max Jacobson <max@hardscrabble.net>
2022-06-10 00:04:38 -0400

    Ruby 3 compatibility
    The fancy stuff did not work under Ruby 3, so au revoir to all that

--- gems.rb
+++ gems.rb
@@ -1,16 +1,7 @@
 source "https://rubygems.org"
 
-begin
-  # this trick via:
-  # https://byparker.com/blog/2014/stay-up-to-date-with-the-latest-github-pages-gem/
-  require 'json'
-  require 'open-uri'
-  versions = JSON.parse(URI('https://pages.github.com/versions.json').open.read)
-  gem 'github-pages', versions.fetch('github-pages')
-rescue SocketError => e
-  STDERR.puts "Couldn't fetch remote github-pages: #{e.inspect}"
-  gem 'github-pages'
-end
+gem 'github-pages'
+gem 'webrick'
 
 group :development do
   gem 'scss_lint'
