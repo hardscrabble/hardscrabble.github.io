c9b5fc0fa5194ca4395eb050dd88ddd2de774756
Max Jacobson <max@hardscrabble.net>
2024-11-06 08:44:20 -0500

    Bump pagefind to 1.2.0

--- _plugins/pagefind.rb
+++ _plugins/pagefind.rb
@@ -1,3 +1,3 @@
 Jekyll::Hooks.register :site, :post_write do |page|
-  system("npx -y pagefind@1.1.1 --site _site", exception: true)
+  system("npx -y pagefind@1.2.0 --site _site", exception: true)
 end
