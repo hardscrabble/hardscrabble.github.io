002631304f18bc38708caa62f2648d67d68b24f2
Max Jacobson <max@hardscrabble.net>
2024-12-17 21:09:43 -0500

    add a package.json
    so that dependabot will update those dependencies and I don't have to do
    it manually

--- _plugins/pagefind.rb
+++ _plugins/pagefind.rb
@@ -1,3 +1,3 @@
 Jekyll::Hooks.register :site, :post_write do |_site|
-  system("npx -y pagefind@1.2.0 --site _site", exception: true)
+  system("node_modules/.bin/pagefind --site _site", exception: true)
 end
